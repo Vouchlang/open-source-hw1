4203171107bdb0242520e73b4c6d02d3102e710f
Vouchlang <vouchlanggm@gmail.com>
2025-10-26 09:30:55 +0700

    add b

---
+++ b.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>  
+    <head>
+        <title>Embedded PHP Example</title>
+    </head>
+    <body>  
+        <form action="">
+            Username: <input type="text">
+            Email: <input type="email">
+        </form>
+    </body>
+</html>
