65107a96fbbc46da03d9866e47357166ecebe221
Vouchlang <vouchlanggm@gmail.com>
2025-10-26 09:35:38 +0700

    update header

---
+++ b.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>  
+    <head>
+        <title>b header</title>
+    </head>
+    <body>  
+        <form action="">
+            Username: <input type="text">
+            Email: <input type="email">
+        </form>
+</html>
