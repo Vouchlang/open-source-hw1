5c824597c23543773d4844ecacb76bd1dd0d3e89
Vouchlang <vouchlanggm@gmail.com>
2025-10-26 11:24:53 +0700

    Revert "remove"
    This reverts commit 44c8518373f466d8cbef956ca23ed1259908adeb.

---
+++ a.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Embedded PHP Example</title>
+</head>
+<body>  
+    <form action="">
+        Username: <input type="text">
+        Email: <input type="email">
+    </form>
+</body>
+<?php
+    echo "Lang";
+    echo "Language";
+?>
