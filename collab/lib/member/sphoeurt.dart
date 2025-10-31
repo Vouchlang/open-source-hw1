8a434d2d5f42f313d296d0175c56971d4ea30f3a
Bee phoeut <beebee9999@gmail.com>
2025-10-31 12:04:27 +0700

    Update Phoeut detail

--- collab/lib/member/sphoeurt.dart
+++ collab/lib/member/sphoeurt.dart
@@ -1,7 +1,7 @@
 const sphoeutName = "SPhoeut";
 const sphoeutUrl = "assets/phoeut.JPG";
-const sphoeutDesc = "";
-const sphoeutMajor = "";
-const sphoeutDegree = "";
-const sphoeutYear = "";
-const sphoeutJob = "";
+const sphoeutDesc = "Hello i'm Phoeut";
+const sphoeutMajor = "IT";
+const sphoeutDegree = "Master";
+const sphoeutYear = "2";
+const sphoeutJob = "Content Creator";
