9e528cd6b599077a71fbca9fd1ffd46351c3fb5f
Liheng <122438825+lihengsomkh@users.noreply.github.com>
2025-10-31 11:57:16 +0700

    Liheng

--- collab/lib/member/lheng.dart
+++ collab/lib/member/lheng.dart
@@ -1,8 +1,8 @@
 const lhengName = "LHeng";
-const lhengUrl = "";
-const lhengDesc = "";
-const lhengMajor = "";
-const lhengDegree = "";
-const lhengYear = "";
-const lhengJob = "";
+const lhengUrl = "assets/liheng.jpg";
+const lhengDesc = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.";
+const mheangMajor = "Information Technology";
+const mheangDegree = "Master Degree";
+const mheangYear = "1";
+const mheangJob = "Web Developer";
 
