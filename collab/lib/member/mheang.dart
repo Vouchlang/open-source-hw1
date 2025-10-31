e331d80caa1c200486ec500b83b51df0a8de3476
SM-Heang <sm.heangg@gmail.com>
2025-10-31 10:36:02 +0700

    update mengheang profile

--- collab/lib/member/mheang.dart
+++ collab/lib/member/mheang.dart
@@ -1,7 +1,8 @@
 const mheangName = "MHeang";
 const mheangUrl = "assets/sm_heang.JPG";
-const mheangDesc = "";
-const mheangMajor = "";
-const mheangDegree = "";
-const mheangYear = "";
-const mheangJob = "";
+const mheangDesc =
+    "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.";
+const mheangMajor = "Information Technology";
+const mheangDegree = "Master Degree";
+const mheangYear = "2";
+const mheangJob = "Web Developer";
