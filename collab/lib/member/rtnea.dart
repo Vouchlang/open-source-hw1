f96baa38163857f5d498252bc392f497db636ce6
Vouchlang <vouchlanggm@gmail.com>
2025-10-31 14:57:21 +0700

    Update from RTN

--- collab/lib/member/rtnea.dart
+++ collab/lib/member/rtnea.dart
@@ -1,8 +1,9 @@
 const rtneaName = "RTNea";
-const rtneaUrl = "";
-const rtneaDesc = "";
-const rtneaMajor = "";
-const rtneaDegree = "";
-const rtneaYear = "";
-const rtneaJob = "";
-
+const rtneaUrl = "assets/rtn.jpg";
+const rtneaDesc = "Ratanie (Krameria triandra) is a medicinal plant native to South America, known for its red roots rich in tannins.
+It’s traditionally used to strengthen gums, soothe sore throats, and treat mouth inflammation.
+Its extract is often found in herbal toothpaste and mouthwash for its natural astringent and antibacterial properties.";
+const rtneaMajor = "Information Technology";
+const rtneaDegree = "Bachelor Degree";
+const rtneaYear = "2020 - 2024";
+const rtneaJob = "Human Resource";
