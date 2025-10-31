57daab1c5227244e37337381565b5eca6e9c50ec
Seyha <seyhaseven7@gmail.com>
2025-10-31 15:17:29 +0700

    Update seyha

--- collab/lib/member/sha.dart
+++ collab/lib/member/sha.dart
@@ -1,7 +1,7 @@
 const shaName = "SHa";
-const shaUrl = "";
-const shaDesc = "";
-const shaMajor = "";
-const shaDegree = "";
-const shaYear = "";
-const shaJob = "";
+const shaUrl = "assets/seyha.jpg";
+const shaDesc = "Seyha is a passionate developer with a keen interest in collaborative projects and open-source contributions.";
+const shaMajor = "Information Technology";
+const shaDegree = "Bachelor's Degree";
+const shaYear = "2017 - 2021";
+const shaJob = "Graphic Designer";
